003402f4ac999ff4a53b630903f021eeff878342
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-25 14:09:57 +0200

    check env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weatherApp';
+
+// je cherche a utiliser le service WeatherApp
+// j'implemente OnInit
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService){
+
+  }
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Wellington') // on créé une instance de class à l'initialisation du projet
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+
+      }
+    })
+  }
+
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // ici, j'importe du fichier d'environnement mes variables https://rapidapi.com/KirylBokiy/api/openweather43/
+  // je passe en params les différentes query que je recherche
+  // j'utilise TS, donc je type mes données en utilisant un modele https://transform.tools/json-to-typescript
+  // on importe le model WeatherData, et la réponse de l'API sera une instance de class
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('appid', environment.appid)
+      .set('appid', environment.appid)
+      .set('units', 'metric')
+      .set('mode', 'json')
+    })
+  }
 }
