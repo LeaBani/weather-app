d5c81b020d51abfc7e0d793366791c212306adf5
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-25 13:51:57 +0200

    test api and create OnInit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: "https://openweather43.p.rapidapi.com/weather",
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '63e1fd48bemshda4ab3d73e87a28p19c543jsn9cc446363fd8',
+  appid: 'da0f9c8d90bde7e619c3ec47766a42f4'
+ }
