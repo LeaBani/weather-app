0b8193e6e1f5fd6880da5750397a12feafdf517f
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-25 13:56:30 +0200

    Revert "test api and create OnInit"
    This reverts commit d5c81b020d51abfc7e0d793366791c212306adf5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService } from './services/weather.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-// je cherche a utiliser le service WeatherApp
-// j'implemente OnInit
-export class AppComponent implements OnInit {
-
-  constructor(private weatherService: WeatherService){
-
-  }
-  ngOnInit(): void {
-    this.weatherService.getWeatherData('Wellington') // on créé une instance de class à l'initialisation du projet
-    .subscribe({
-      next: (response) => {
-        console.log(response);
-
-      }
-    })
-  }
-
+export class AppComponent {
+  title = 'weatherApp';
 }
